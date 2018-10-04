bc00aae958b5b535ead6d53a91428fd2b3799677
ypadron <ypadron82@gmail.com>
2018-10-04 16:11:55 -0400

    fix: Remove .focus() from View Details link

--- js/main.js
+++ js/main.js
@@ -194,7 +194,8 @@ let createRestaurantHTML = (restaurant) => {
 	address.innerHTML = restaurant.address;
 	li.append(address);
 
-	const more = document.createElement('a').focus();
+	// Removed .focus() b/c it created bug & 'view details' focus' properly w/o added method.
+	const more = document.createElement('a');
 	more.innerHTML = 'View Details';
 	more.href = DBHelper.urlForRestaurant(restaurant);
 	li.append(more);
