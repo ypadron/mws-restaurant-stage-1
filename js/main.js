8060199ab8cc9bfc4748c5ae562cb3356db24bc1
ypadron <ypadron82@gmail.com>
2018-10-04 17:41:54 -0400

    feat: Add alt attribute to images for a11y

--- js/main.js
+++ js/main.js
@@ -180,6 +180,7 @@ let createRestaurantHTML = (restaurant) => {
 	const image = document.createElement('img');
 	image.className = 'restaurant-img';
 	image.src = DBHelper.imageUrlForRestaurant(restaurant);
+	image.setAttribute('alt', restaurant.name + ' ' + 'in ' + restaurant.neighborhood);
 	li.append(image);
 
 	const name = document.createElement('h1');
